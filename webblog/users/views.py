0cef075c6b32e9d5a8e78515f9cf04de2931c461
Bassel <bassel@bld.ai>
2021-12-25 21:52:58 +0200

    Created the base login for Sign-Up view.

--- webblog/users/views.py
+++ webblog/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import MyUser
+from .forms import UserSignUpForm
+
+
+class SignUp(CreateView):
+    form_class = UserSignUpForm
+    success_url = reverse_lazy('home')
+    template_name = 'account/sign-up.html'
+
+    def from_valid(self, form, commit=True):
+
+        user = form.save(commit=False)
+        user.set_password(form.cleaned_data['password1'])
+
+        if commit:
+            user.save()
+        return super().from_valid(form)
