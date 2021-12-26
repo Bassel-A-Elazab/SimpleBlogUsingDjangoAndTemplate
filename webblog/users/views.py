a4c2c9ca7df024c87809f8d1c2046bed1d49ddca
Bassel <bassel@bld.ai>
2021-12-26 14:56:31 +0200

    Sending a token via email for activating the user's account.

--- webblog/users/views.py
+++ webblog/users/views.py
@@ -1,8 +1,13 @@
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
 
 from .models import MyUser
 from .forms import UserSignUpForm
+from .tokens import account_activation_token
 
 
 class SignUp(CreateView):
@@ -17,4 +22,18 @@ class SignUp(CreateView):
 
         if commit:
             user.save()
+            current_site = get_current_site(self.request)
+            subject = 'Activate your account'
+            message = render_to_string('account/account-activation-email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': force_str(urlsafe_base64_encode(force_bytes(user.pk))),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject, message)
+
         return super().from_valid(form)
+
+
+def activate(request, uid, token):
+    pass
