39033c589b9fafe29b2d408ccd1bf46ec8206183
Bassel <bassel@bld.ai>
2021-12-28 23:33:40 +0200

    Handled Sign-Up Handled the user's registration and account activation with a basic design.

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
+    path('users/', include('webblog.users.urls')),
+    path('users/', include('django.contrib.auth.urls')),
+    path('social-api/users/auth/', include('allauth.urls')),
+    path('', TemplateView.as_view(template_name='blog/home.html'), name='home')
 ]
--- webblog/users/views.py
+++ webblog/users/views.py
@@ -1,9 +1,12 @@
 from django.views.generic import CreateView
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
+
 
 from .models import MyUser
 from .forms import UserSignUpForm
@@ -13,9 +16,9 @@ from .tokens import account_activation_token
 class SignUp(CreateView):
     form_class = UserSignUpForm
     success_url = reverse_lazy('home')
-    template_name = 'account/sign-up.html'
+    template_name = 'account/signup.html'
 
-    def from_valid(self, form, commit=True):
+    def form_valid(self, form, commit=True):
 
         user = form.save(commit=False)
         user.set_password(form.cleaned_data['password1'])
@@ -24,16 +27,27 @@ class SignUp(CreateView):
             user.save()
             current_site = get_current_site(self.request)
             subject = 'Activate your account'
-            message = render_to_string('account/account-activation-email.html', {
+            message = render_to_string('account/email_confirm.html', {
                 'user': user,
                 'domain': current_site.domain,
                 'uid': force_str(urlsafe_base64_encode(force_bytes(user.pk))),
                 'token': account_activation_token.make_token(user),
             })
             user.email_user(subject, message)
-
-        return super().from_valid(form)
+        return super().form_valid(form)
 
 
 def activate(request, uid, token):
-    pass
+    try:
+        _uid = force_str(urlsafe_base64_decode(uid))
+        user = MyUser.objects.get(pk=_uid)
+    except (TypeError, ValueError, OverflowError, MyUser.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect('home')
+    else:
+        return render(request, 'account/account_verification_invalid_notice.html')
