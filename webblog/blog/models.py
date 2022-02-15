421b06a0250ab363e596e7dd6cb0182da070edb0
Bassel <bassel@bld.ai>
2022-02-15 23:47:12 +0200

    Defined the basic fields for the blog model

--- webblog/blog/models.py
+++ webblog/blog/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.db import models 
 
-# Create your models here.
+from datetime import datetime
+from webblog.users.models import MyUser
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(MyUser, on_delete=models.SET_NULL, null=True)
+    description = models.TextField(max_length=2000, help_text="Enter you blog text here.")
+    post_date = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        ordering = ["-post_date"]
+    
