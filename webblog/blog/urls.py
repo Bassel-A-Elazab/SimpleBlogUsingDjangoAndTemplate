3718a27156061e693548d45ee61dc688f8060f72
Bassel <bassel@bld.ai>
2022-02-28 23:49:33 +0200

    Setup the BlogComment form and created its template for allowing users to add their comments.

--- webblog/blog/urls.py
+++ webblog/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import BlogListView, BlogDetailView
+from .views import BlogCommentCreateView, BlogListView, BlogDetailView
 
 urlpatterns = [
     path('blogs/', BlogListView.as_view(), name='blogs'),
     path('blogs/<pk>', BlogDetailView.as_view(), name='blog-detail'),
+    path('blogs/<pk>/comment', BlogCommentCreateView.as_view(), name='add-comment'),
 ]
--- webblog/blog/views.py
+++ webblog/blog/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
 from django.views.generic import ListView
 from django.views.generic import DetailView
+from django.views.generic.edit import CreateView
 
-from .models import Blog
+from .models import Blog, BlogComment
 
 
 class BlogListView(ListView):
@@ -11,3 +15,16 @@ class BlogListView(ListView):
 
 class BlogDetailView(DetailView):
     model = Blog
+
+
+class BlogCommentCreateView(LoginRequiredMixin, CreateView):
+    model = BlogComment
+    fields = ['comment']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.blog = get_object_or_404(Blog, pk=self.kwargs['pk'])
+        return super(BlogCommentCreateView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('blog-detail', kwargs={'pk': self.kwargs['pk'], })
