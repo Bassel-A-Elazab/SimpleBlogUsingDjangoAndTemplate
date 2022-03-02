f3d1ad90dd2d20fb780b12294a76af513b270f28
basola <bassel.alazab@gmail.com>
2022-03-03 00:11:18 +0200

    Removed wrong test for now until I'll fix and add another ones

--- webblog/blog/tests/test_views.py
+++ webblog/blog/tests/test_views.py
@@ -2,10 +2,6 @@ from django.test import TestCase
 from django.urls import reverse
 
 
-from webblog.blog.models import Blog
-from webblog.users.models import MyUser
-
-
 class BlogListViewTest(TestCase):
 
     def test_view_url_exists_at_desired_location(self):
@@ -20,21 +16,3 @@ class BlogListViewTest(TestCase):
         response = self.client.get(reverse('blogs'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'blog/blog_list.html')
-
-class BlogCommentViewTest(TestCase):
-
-    @classmethod
-    def setUpTestData(self):
-        self.test_user1 = MyUser.objects.create_superuser(email='test1@user.com', name='test1', password='Test123')
-        self.blog = Blog.objects.create(title='Blog 1 title', description='Blog 1 description', author=self.test_user1)
-
-    def test_view_get_correct_context(self):
-        self.client.force_login(self.test_user1)
-
-        data = {
-            'comment': 'Blog 1 comment',
-            'blog': self.blog,
-            'author': self.test_user1
-        }
-        response = self.client.post(reverse('add-comment', args=[self.blog.pk]), data=data)
-        self.assertEqual(response.status_code, 200)
