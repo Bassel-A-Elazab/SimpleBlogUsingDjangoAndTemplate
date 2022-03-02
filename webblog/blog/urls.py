a2a30b6a7cf58601ee7b3b27cd961db5950749f0
basola <bassel.alazab@gmail.com>
2022-03-03 00:20:24 +0200

    Updated our URL pathes

--- webblog/blog/urls.py
+++ webblog/blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import BlogCommentCreateView, BlogListView, BlogDetailView
 
 urlpatterns = [
-    path('blogs/', BlogListView.as_view(), name='blogs'),
+    path('', BlogListView.as_view(), name='blogs'),
     path('blogs/<int:pk>', BlogDetailView.as_view(), name='blog-detail'),
-    path('blogs/<int:pk>/comment', BlogCommentCreateView.as_view(), name='add-comment'),
+    path('blogs/<int:pk>/comment/', BlogCommentCreateView.as_view(), name='add-comment'),
 ]
