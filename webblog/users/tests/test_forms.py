37cbdefd4d8b3093165c138614f3d9bd4d0cf72f
basola <bassel.alazab@gmail.com>
2022-03-11 21:06:42 +0200

    Restructured, organized, and updated our user forms and unit tested  it

--- webblog/users/tests/test_forms.py
+++ webblog/users/tests/test_forms.py
@@ -4,9 +4,8 @@ from django.utils.translation import gettext_lazy as _
 from webblog.users.forms import UserCreationFrom, UserSignUpForm
 
 
-class MyUserFormsTests(TestCase):
-
-    def test_clean_password2(self):
+class UserCreationFromTest(TestCase):
+    def test_clean_password2_creation_form_fail(self):
         form = UserCreationFrom(
             {
                 'email': 'test@user.com',
@@ -20,43 +19,61 @@ class MyUserFormsTests(TestCase):
         self.assertEqual(form.errors['password2']
                          [0], _("Password don't match!"))
 
-    def test_custom_from(self):
+    def test_clean_password2_creation_form_pass(self):
         form = UserCreationFrom(
             {
                 'email': 'test@user.com',
                 'name': 'test',
-                'password1': 'test',
-                'password2': 'test',
+                'password1': 'test123',
+                'password2': 'test123',
             }
         )
         self.assertTrue(form.is_valid())
-        if form.is_valid():
-            form.save(commit=False)
-            if form.save(commit=True):
-                user = form.save(commit=False)
-                user.save()
 
-    def test_clean_password2_with_signup(self):
-        form = UserSignUpForm(
+    def test_save_custom_form_success(self):
+        form = UserCreationFrom(
             {
                 'email': 'test@user.com',
                 'name': 'test',
-                'password1': 'test',
+                'password1': 'test123',
                 'password2': 'test123',
             }
         )
+        self.assertTrue(form.is_valid())
+        user = form.save()
+        self.assertEqual(user.email, 'test@user.com')
+        self.assertEqual(user.name, 'test')
+
+
+class UserSignUpFormTest(TestCase):
+    @classmethod
+    def setUpTestData(self):
+        email = 'test@user.com'
+        name = 'test'
+        password1 = 'test123'
+        password2 = 'test123'
+        password2_wrong = 'test'
+        self.form_data_correct = {
+            'email': email,
+            'name': name,
+            'password1': password1,
+            'password2': password2,
+        }
+
+        self.form_data_wrong = {
+            'email': email,
+            'name': name,
+            'password1': password1,
+            'password2': password2_wrong,
+        }
+
+    def test_clean_password2_signup_fail(self):
+        form = UserSignUpForm(self.form_data_wrong)
 
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['password2']
                          [0], _("Password don't match!"))
 
-    def test_custom_from_with_signup(self):
-        form = UserSignUpForm(
-            {
-                'email': 'test@user.com',
-                'name': 'test',
-                'password1': 'test',
-                'password2': 'test',
-            }
-        )
+    def test_clean_password2_signup_pass(self):
+        form = UserSignUpForm(self.form_data_correct)
         self.assertTrue(form.is_valid())
