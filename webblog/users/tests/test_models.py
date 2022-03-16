7b99cfef7f592ea079f550ee5e191c2b1b2c96b7
basola <bassel.alazab@gmail.com>
2022-03-16 23:26:44 +0200

    Added the URL to access a particular blogger-detail instance.

--- webblog/users/tests/test_models.py
+++ webblog/users/tests/test_models.py
@@ -1,24 +1,19 @@
 from django.test import TestCase
-from django.core import mail
+from django.urls import reverse
 
 from webblog.users.models import MyUser
 
 
 class MyUserModelsTests(TestCase):
 
+    @classmethod
+    def testSetUpDataTest(self):
+        self.test_user1 = MyUser.objects.create_user(
+            email='test@user.com', name='test', password='test123')
+
     def test_str_is_equal_to_email(self):
-        my_user = MyUser.objects.create(
-            email='test@user.com', name='test', password='test123')
-        self.assertEqual(str(my_user), 'test@user.com')
+        self.assertEqual(str(self.test_user1), 'test@user.com')
 
-    def test_send_email(self):
-        my_user = MyUser.objects.create(
-            email='test@user.com', name='test', password='test123')
-        subject = 'Test Subject'
-        body = 'Test Body'
-
-        my_user.email_user(subject, body, my_user.email)
-        self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].subject, 'Test Subject')
-        self.assertEqual(mail.outbox[0].body, 'Test Body')
-        self.assertEqual(mail.outbox[0].to, [my_user.email])
+    def test_get_desired_absolute_url(self):
+        expected_url = '/bloggers/' + str(self.test_user1.pk)
+        self.assertEqual(self.test_user1.get_absolute_url(), expected_url)
