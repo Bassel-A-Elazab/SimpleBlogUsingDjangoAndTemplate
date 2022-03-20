dab713bdf641547aa0054d4be3f2de18e15bdb2f
basola <bassel.alazab@gmail.com>
2022-03-20 22:02:23 +0200

    Removed unused library

--- webblog/users/views.py
+++ webblog/users/views.py
@@ -1,4 +1,3 @@
-from pyexpat import model
 from django.views.generic import ListView
 from django.views.generic import DetailView
 
@@ -10,8 +9,8 @@ class BloggerListView(ListView):
     template_name = "blog/blog_author_list.html"
     context_object_name = "blogger_list"
 
+
 class BloggerDetailView(DetailView):
     model = MyUser
     template_name = "blog/blog_author_detail.html"
     context_object_name = "blogger"
-
