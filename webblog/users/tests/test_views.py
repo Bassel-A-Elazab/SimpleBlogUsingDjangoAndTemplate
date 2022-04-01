f9860b230c9fd0b2d1fae70d33cfce1cf4069fa0
basola <bassel.alazab@gmail.com>
2022-04-01 22:38:31 +0200

    The wrong test was modified with the user's reference updated.

--- webblog/users/tests/test_views.py
+++ webblog/users/tests/test_views.py
@@ -1,16 +1,17 @@
-from django.urls import reverse
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-from webblog.users.models import MyUser
 from webblog.blog.models import Blog
 
 
 class BloggerListViewTest(TestCase):
     @classmethod
     def setUpTestData(self):
+        User = get_user_model()
         number_of_users = 13
         for user_num in range(number_of_users):
-            MyUser.objects.create_user(
+            User.objects.create_user(
                 email='test%s@user.com' % user_num, name='test%s' % user_num, password='test123')
 
     def test_view_url_exists_at_desired_location(self):
@@ -42,7 +43,8 @@ class BloggerDetailViewTest(TestCase):
 
     @classmethod
     def setUpTestData(self):
-        self.test_user1 = MyUser.objects.create_user(
+        User = get_user_model()
+        self.test_user1 = User.objects.create_user(
             email='test@user.com', name='test', password='test123')
         self.blog = Blog.objects.create(
             title='Test Blog 1', author=self.test_user1, description='Test Blog 1 Description')
@@ -71,12 +73,11 @@ class BloggerDetailViewTest(TestCase):
             reverse('blogger-detail', kwargs={"pk": self.test_user1.pk}))
 
         expected_blog_context_object_name = "blogger_blog_list"
-        expected_blog_title = "Test Blog 1"
-        expected_blog_description = "Test Blog 1 Description"
+        expected_blog_title = "Test Blog 12"
+        expected_blog_description = "Test Blog 12 Description"
         expected_blog_author = self.test_user1
 
         output_blogger_blog = response.context_data['blogger_blog_list'][0]
-
         self.assertEqual(response.status_code, 200)
         self.assertIn(expected_blog_context_object_name, response.context)
         self.assertEqual(expected_blog_title, output_blogger_blog.title)
@@ -86,7 +87,6 @@ class BloggerDetailViewTest(TestCase):
 
     def test_pagination_is_five(self):
         response = self.client.get(
-            reverse('blogger-detail'), kwargs={"pk": self.test_user1.pk})
+            reverse('blogger-detail', kwargs={"pk": self.test_user1.pk}))
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(response.context['is_paginated'] == True)
         self.assertEqual(len(response.context['blogger_blog_list']), 5)
