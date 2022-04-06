e278116bbf2840a693c6b27c1162bb279d4734dd
basola <bassel.alazab@gmail.com>
2022-04-06 23:49:31 +0200

    New url for blogger settings

--- webblog/users/urls.py
+++ webblog/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import BloggerListView, BloggerDetailView
+from .views import BloggerListView, BloggerDetailView, BloggerUpdateView
 
 urlpatterns = [
     path('bloggers/', BloggerListView.as_view(), name='bloggers'),
     path('bloggers/<int:pk>', BloggerDetailView.as_view(), name='blogger-detail'),
+    path('bloggers/<int:pk>/settings', BloggerUpdateView.as_view(), name='blogger-settings')
 ]
