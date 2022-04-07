0bebb85a3a35644a7c1c3b43a715b43f471ec884
basola <bassel.alazab@gmail.com>
2022-04-07 23:36:34 +0200

    Providing users with the ability to update their information

--- webblog/users/views.py
+++ webblog/users/views.py
@@ -1,7 +1,10 @@
 from django.core.paginator import Paginator
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
-from django.views.generic import ListView
-from django.views.generic import DetailView
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, UpdateView
+
+from .forms import BloggerUpdateForm
 
 from .models import MyUser
 
@@ -25,10 +28,20 @@ class BloggerDetailView(DetailView):
             *args, **kwargs)
         self.blogger = get_object_or_404(MyUser, pk=self.kwargs['pk'])
         blog_list = Blog.objects.filter(author=self.blogger)
-        
+
         paginator = Paginator(blog_list, 5)
         page_number = self.request.GET.get('page')
         page_obj = paginator.get_page(page_number)
-        
+
         context['blogger_blog_list'] = page_obj
         return context
+
+
+class BloggerUpdateView(LoginRequiredMixin, UpdateView):
+    model = MyUser
+    form_class = BloggerUpdateForm
+    template_name = 'users/settings.html'
+
+    def get_success_url(self):
+        pk = self.kwargs["pk"]
+        return reverse("blogger-settings", kwargs={"pk": pk})
