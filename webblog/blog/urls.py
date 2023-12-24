5bfc8ea68cfda7f3dc64a2e6eb30292b57658147
Bassel Elazab <bassel@bld.ai>
2023-12-24 17:16:03 +0200

    Implement an API endpoint to retrieve a list of tags for the explore page.  - Create an API endpoint that fetches details for a specific tag.  - Implement pagination for blogs related to a specific tag.

--- webblog/blog/urls.py
+++ webblog/blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import BlogListView, BlogDetailCommentView
+from .views import BlogListView, BlogDetailCommentView, BlogTagDetail, BlogTagList
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='blogs'),
     path('blogs/<int:pk>', BlogDetailCommentView.as_view(), name='blog-detail'),
+    path('tags/', BlogTagList.as_view(), name="tag-list"),
+    path('tags/<int:pk>', BlogTagDetail.as_view(), name='tag-detail'),
 ]
