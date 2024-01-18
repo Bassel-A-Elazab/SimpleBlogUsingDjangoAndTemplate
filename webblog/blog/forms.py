bc5c036079a3ed48365ec9f9145832ebcba2d65e
Bassel Elazab <bassel.alazab@gmail.com>
2024-01-18 22:20:47 +0200

    Create a new form for a new blog post.

--- webblog/blog/forms.py
+++ webblog/blog/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 
-from .models import BlogComment
+from .models import Blog, BlogComment
+
+
+class BlogCreateForm(forms.ModelForm):
+    class Meta:
+        model = Blog 
+        fields = ['title', 'description', 'cover', 'tags']
 
 
 class BlogCommentForm(forms.ModelForm):
