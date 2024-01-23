0898d3dceb825721b94434dde1b8d9068fb69ee2
Bassel Elazab <bassel.alazab@gmail.com>
2024-01-23 19:22:30 +0200

    Add SummernoteWidget to the blog's description field.

--- webblog/blog/forms.py
+++ webblog/blog/forms.py
@@ -1,13 +1,17 @@
 from django import forms
+from django_summernote.widgets import SummernoteWidget
 
 from .models import Blog, BlogComment
 
 
 class BlogCreateForm(forms.ModelForm):
+
     class Meta:
         model = Blog 
         fields = ['title', 'description', 'cover', 'tags']
-
+        widgets = {
+            'description': SummernoteWidget(),
+        }
 
 class BlogCommentForm(forms.ModelForm):
     comment = forms.CharField(
