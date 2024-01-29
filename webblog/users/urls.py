f3085d020c35d1c26e5ee0cc3f30ac0b8cf34a41
Bassel Elazab <bassel.alazab@gmail.com>
2024-01-29 18:01:17 +0200

    Make a new DetailView to return all user information on the updated dashboard page.  - Make unit tests for that view.

--- webblog/users/urls.py
+++ webblog/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import BloggerListView, BloggerDetailView, BloggerUpdateView
+from .views import BloggerDashboardView, BloggerListView, BloggerDetailView, BloggerUpdateView
 
 urlpatterns = [
     path('bloggers/', BloggerListView.as_view(), name='bloggers'),
     path('bloggers/<int:pk>', BloggerDetailView.as_view(), name='blogger-detail'),
-    path('bloggers/<int:pk>/settings', BloggerUpdateView.as_view(), name='blogger-settings')
+    path('bloggers/<int:pk>/settings', BloggerUpdateView.as_view(), name='blogger-settings'),
+    path('bloggers/<int:pk>/dashboard', BloggerDashboardView.as_view(), name='blogger-dashboard'),
 ]
